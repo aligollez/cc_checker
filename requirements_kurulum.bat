d28846c00b6ead96277f96dc54fcf240e9efc183
Aligollez <59908197+aligollez@users.noreply.github.com>
2022-01-15 04:21:40 +0300

    Update requirements_kurulum.bat

--- requirements_kurulum.bat
+++ requirements_kurulum.bat
@@ -3,3 +3,10 @@ pip install chromedriver_autoinstaller
 pip install selenium
 pip install colorama
 pip install webdriver
+pip3 install chromedriver_autoinstaller
+pip3 install selenium
+pip3 install colorama
+pip3 install webdriver
+
+
+
